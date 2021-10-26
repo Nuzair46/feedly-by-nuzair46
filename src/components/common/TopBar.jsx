ba3220a515dc6607ce923e340d3f36ae7b077bf0
Nuzair46 <nuzer501@gmail.com>
2021-10-26 16:03:46 +0530

    topbar

--- src/components/common/TopBar.jsx
+++ src/components/common/TopBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Filter } from "@bigbinary/neeto-icons";
+import { Filter, Search, Notification } from "@bigbinary/neeto-icons";
 import { Button } from "neetoui";
 import { Header } from "neetoui/layouts";
 
@@ -9,7 +9,17 @@ function TopBar() {
     <div className="border-b px-6">
       <Header
         title="Feed.ly"
-        actionBlock={<Button label="Filter" icon={Filter} style="secondary" />}
+        actionBlock={
+          <div className="space-x-3">
+            <div className="inline-block align-middle">
+              <div className="flex space-x-2">
+                <Search size={24} />
+                <Notification size={24} />
+              </div>
+            </div>
+            <Button label="Filter" icon={Filter} style="secondary" />
+          </div>
+        }
       />
     </div>
   );
