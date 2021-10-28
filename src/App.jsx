81a17ec337633d359ce462951167a7c7b1c568c2
Nuzair46 <nuzer501@gmail.com>
2021-10-28 19:08:18 +0530

    added promise

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,64 @@
 import React, { useState, useEffect } from "react";
-
+/*
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { PageLoader } from "neetoui";
 import TopBar from "./components/Common/TopBar";
 import Dashboard from "./components/Dashboard";
 import ArticlePane from "./components/Dashboard/ArticlePane";
-import { API } from "./api/axios";
 import ErrorBound from "./components/Common/ErrorBound";
+*/
+
+import { PageLoader } from "neetoui";
+import { API } from "./api/axios";
+import { CATEGORY } from "./components/Common/constants";
 
 function App() {
   const [NEWS, setNews] = useState({});
   const [loading, setLoading] = useState(true);
   useEffect(() => {
+    //fetchNews();
     apiCall();
   }, []);
 
-  const apiCall = async () => {
-    try {
-      let response = await API.get(`/news?category=science`); //${item.toLowerCase()}`);
-      setNews(response.data);
-      localStorage.setItem("newsdata", JSON.stringify(response.data));
-      setLoading(false);
-    } catch (error) {
-      true;
-    } finally {
-      true;
-    }
+  const apiCall = () => {
+    Promise.all(
+      CATEGORY.map(category =>
+        API.get(`/news?category=${category.toLowerCase()}`)
+      )
+    )
+      .then(responses => {
+        return Promise.all(responses.map(response => response.data));
+      })
+      .then(data => {
+        setNews(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
-
+  //async-await method. has problems
+  /*
+  const fetchNews = () => {
+    CATEGORY.map(async category => {
+      try {
+        let response = await API.get(`${category.toLowerCase()}`);
+        let resp = NEWS;
+        resp[category] = response.data;
+        setNews(resp);
+        console.log(NEWS);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+  */
   if (loading) {
     return <PageLoader />;
   }
 
+  return <div>{JSON.stringify(NEWS)}</div>;
+  /*
   return (
     <BrowserRouter>
       <TopBar />
@@ -58,6 +84,7 @@ function App() {
       </Switch>
     </BrowserRouter>
   );
+  */
 }
 
 export default App;
