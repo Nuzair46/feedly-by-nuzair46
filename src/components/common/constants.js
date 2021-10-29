752b1c2d3749bb2fadc5d9a39a380e3db85757d9
Nuzair46 <nuzer501@gmail.com>
2021-10-30 04:16:54 +0530

    using formik on subscribe

--- src/components/common/constants.js
+++ src/components/common/constants.js
@@ -30,9 +30,12 @@ export const FILTER = {
   automobile: false,
 };
 
-export const LOREM_IPSUM =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
 if (!JSON.parse(localStorage.getItem("filter"))) {
   localStorage.setItem("filter", JSON.stringify(FILTER));
 }
+
+export const LOREM_IPSUM =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+export const WEBHOOK_URL =
+  "https://webhook.site/8eb4c5e7-b24c-4b81-b414-431cfd3f56f4";
