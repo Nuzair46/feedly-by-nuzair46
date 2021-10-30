ea1034d1195fbd64ae8e576ed5b1b02b1d87b07a
Nuzair46 <nuzer501@gmail.com>
2021-10-30 14:31:00 +0530

    Added webhook post for query

--- src/components/Dashboard/EmptyModal.jsx
+++ src/components/Dashboard/EmptyModal.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Modal, Typography, Button, Input, Textarea } from "neetoui";
+import axios from "axios";
+import { WEBHOOK_URL } from "../common/constants";
 
 function EmptyModal({ showModal, setShowModal }) {
+  const [nameValue, setNameValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [messageValue, setMessageValue] = useState("");
+
+  const onSubmit = () => {
+    axios.post(WEBHOOK_URL, {
+      query: {
+        name: nameValue,
+        email: emailValue,
+        message: messageValue,
+      },
+    });
+  };
   return (
     <div className="w-full">
       <Modal
@@ -22,30 +37,52 @@ function EmptyModal({ showModal, setShowModal }) {
             </Typography>
           </div>
         </Modal.Header>
-        <Modal.Body>
-          <div className="flex space-x-4 w-11/12 my-4">
-            <Input label="Name" placeholder="Oliver Smith" />
-            <Input label="Email" placeholder="oliver@example.com" />
-          </div>
-          <Textarea
-            className="w-11/12"
-            label="Message"
-            placeholder="Write your message here."
-          />
-        </Modal.Body>
-        <Modal.Footer className="space-x-2">
-          <Button
-            label="Submit"
-            onClick={() => setShowModal(false)}
-            size="large"
-          />
-          <Button
-            style="text"
-            label="Cancel"
-            onClick={() => setShowModal(false)}
-            size="large"
-          />
-        </Modal.Footer>
+        <form>
+          <Modal.Body>
+            <div className="flex space-x-4 w-11/12 my-4">
+              <Input
+                label="Name"
+                placeholder="Oliver Smith"
+                name="name"
+                value={nameValue}
+                onChange={e => setNameValue(e.target.value)}
+              />
+              <Input
+                label="Email"
+                placeholder="oliver@example.com"
+                name="email"
+                value={emailValue}
+                onChange={e => setEmailValue(e.target.value)}
+              />
+            </div>
+            <Textarea
+              className="w-11/12"
+              label="Message"
+              placeholder="Write your message here."
+              name="message"
+              value={messageValue}
+              onChange={e => setMessageValue(e.target.value)}
+            />
+          </Modal.Body>
+          <Modal.Footer className="space-x-2">
+            <Button
+              label="Submit"
+              type="submit"
+              onClick={e => {
+                e.preventDefault();
+                onSubmit();
+                setShowModal(false);
+              }}
+              size="large"
+            />
+            <Button
+              style="text"
+              label="Cancel"
+              onClick={() => setShowModal(false)}
+              size="large"
+            />
+          </Modal.Footer>
+        </form>
       </Modal>
     </div>
   );
